745d4b27bb43d5ceb94a13f15153858b7c9d5240
Ramees M Siyad <rameessiyad26@gmail.com>
2024-07-02 08:50:28 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const display = document.getElementById('display');
+
+// const appendToDisplay = (input) => {
+//     display.value += input
+// }
+
+const appendToDisplay = (input) => {
+    display.value += input
+}
+
+const clearDisplay = () => {
+    display.value = ""
+}
+
+// const calculate = () => {
+//     try {
+//         display.value = eval(display.value)
+//     } catch (error) {
+//         display.value = "Error";
+//     }
+// }
+
+// const calculate = () => {
+//    display.value =  eval(display.value)
+// }
+
+function calculate(){
+    display.value = eval(display.value)
+}
